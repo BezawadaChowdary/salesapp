04e4ac6ccb56fc372613dd7e74d1b3f779564c64
vijay <root@ip-172-31-27-91.us-east-2.compute.internal>
2018-02-09 17:51:07 +0000

    restored file from third commit and pushed again, sixth comit

--- helloworld.java
+++ helloworld.java
@@ -3,8 +3,6 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
-	// prints hello git
 	System.out.println("Hello Git");
-	System.out.println("Hello jav");
    }
 }
